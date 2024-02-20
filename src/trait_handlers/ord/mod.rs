7b9a95c6f234b027125b42b01c89496a124b879b
Ian Jackson <ijackson@chiark.greenend.org.uk>
2024-02-20 17:33:07 +0000

    Ord: Fix build when PartialOrd is not enabled

--- src/trait_handlers/ord/mod.rs
+++ src/trait_handlers/ord/mod.rs
@@ -33,13 +33,17 @@ impl TraitHandler for OrdHandler {
     }
 }
 
-fn supertraits(traits: &[Trait]) -> Vec<proc_macro2::TokenStream> {
+fn supertraits(
+    #[allow(unused_variables)]
+    traits: &[Trait],
+) -> Vec<proc_macro2::TokenStream> {
     let mut supertraits = vec![];
     supertraits.push(quote! {::core::cmp::Eq});
 
     // We mustn't add the PartialOrd bound to the educed PartialOrd impl.
     // When we're educing PartialOrd we can leave it off the Ord impl too,
     // since we *know* Self is going to be PartialOrd.
+    #[cfg(feature = "PartialOrd")]
     if !traits.contains(&Trait::PartialOrd) {
         supertraits.push(quote! {::core::cmp::PartialOrd});
     };
