a3965f1a8972f964ce5f11ef66b467e3792832c8
Ian Jackson <ijackson@chiark.greenend.org.uk>
2024-02-26 17:33:41 +0000

    Fix leaking trait bounds
    Replace modification of `ast.generics` with cloning, and then
    modification.  (the Into impl already does things this way.)
    
    Every one of the 18 hunks is identical.
    
    Fixes #23

--- src/trait_handlers/clone/clone_union.rs
+++ src/trait_handlers/clone/clone_union.rs
@@ -42,13 +42,14 @@ impl TraitHandler for CloneUnionHandler {
             &[],
         );
 
-        let where_clause = ast.generics.make_where_clause();
+        let mut generics = ast.generics.clone();
+        let where_clause = generics.make_where_clause();
 
         for where_predicate in bound {
             where_clause.predicates.push(where_predicate);
         }
 
-        let (impl_generics, ty_generics, where_clause) = ast.generics.split_for_impl();
+        let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
         token_stream.extend(quote! {
             impl #impl_generics ::core::clone::Clone for #ident #ty_generics #where_clause {
