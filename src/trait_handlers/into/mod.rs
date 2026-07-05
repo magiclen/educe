8c8ee7c0ad959215259fb41fe8091263e81425f1
Magic Len <len@magiclen.org>
2026-07-05 14:15:34 +0800

    improve the Into trait
    Use BTreeMap instead of HashMap so generated Into impl blocks have a
    stable order.
    
    Refs: #49
    
    Co-authored-by: pchikku <276099616+pchikku@users.noreply.github.com>

--- src/trait_handlers/into/mod.rs
+++ src/trait_handlers/into/mod.rs
@@ -1,3 +1,4 @@
+use crate::trait_handlers::TraitHandlerContext;
 mod common;
 mod into_enum;
 mod into_struct;
@@ -9,22 +10,28 @@ use syn::{Data, DeriveInput, Meta};
 use super::TraitHandlerMultiple;
 use crate::Trait;
 
+/// Dispatches the `Into` derive to the specialized handler for the shape of the input (struct, enum, or union).
 pub(crate) struct IntoHandler;
 
 impl TraitHandlerMultiple for IntoHandler {
     #[inline]
     fn trait_meta_handler(
         ast: &DeriveInput,
+        ctx: &mut TraitHandlerContext,
         token_stream: &mut proc_macro2::TokenStream,
         traits: &[Trait],
         meta: &[Meta],
     ) -> syn::Result<()> {
         match ast.data {
-            Data::Struct(_) => {
-                into_struct::IntoStructHandler::trait_meta_handler(ast, token_stream, traits, meta)
-            },
+            Data::Struct(_) => into_struct::IntoStructHandler::trait_meta_handler(
+                ast,
+                ctx,
+                token_stream,
+                traits,
+                meta,
+            ),
             Data::Enum(_) => {
-                into_enum::IntoEnumHandler::trait_meta_handler(ast, token_stream, traits, meta)
+                into_enum::IntoEnumHandler::trait_meta_handler(ast, ctx, token_stream, traits, meta)
             },
             Data::Union(_) => {
                 Err(crate::panic::trait_not_support_union(meta[0].path().get_ident().unwrap()))
